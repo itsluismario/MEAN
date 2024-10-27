44d10d01fbfa966e19acce6cf4510a7c4c07e36b
itsluismario <itsluismario@gmail.com>
2024-10-26 20:24:47 -0600

    ✅ Add local reference for textarea input
    - Updated onAddPost() to accept HTMLTextAreaElement parameter
    - Added template reference variable #postInput to textarea
    - Pass textarea element to onAddPost() on button click
    - Keep two-way binding with newPost variable

--- src/app/pages/post-create/post-create.component.ts
+++ src/app/pages/post-create/post-create.component.ts
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
 export class PostCreateComponent {
   newPost = "NO CONTENT";
 
-  onAddPost() {
-    this.newPost = 'The user\'s post'
+  onAddPost(inputPost: HTMLTextAreaElement) {
+    this.newPost = inputPost.value
   }
 }
