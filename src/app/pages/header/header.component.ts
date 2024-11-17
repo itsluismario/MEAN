853a17ea759476004dd020f7ffb2345e0fde80af
itsluismario <itsluismario@gmail.com>
2024-11-16 19:17:52 -0600

    🔨 Implement login functionality in the header component
    Changes included in this commit:
    
    1. Updated the `auth.service.ts` file:
       - Added a `getToken()` method to retrieve the stored authentication token.
       - Added a `getAuthStatusListener()` method to provide an observable for the user's authentication status.
       - Implemented the `login()` method, which sends a request to the backend to authenticate the user and stores the received token.
    
    2. Updated the `header.component.ts` file:
       - Subscribed to the `authStatusListener` observable from the `AuthService` to track the user's authentication status.
       - Updated the `userIsAuthenticated` property based on the authentication status.
       - Imported the `CommonModule` to enable the use of the `*ngIf` directive in the template.
    
    3. Updated the `header.component.html` file:
       - Conditionally rendered the login, logout, and post creation links based on the user's authentication status using the `*ngIf` directive.
       - Added the necessary click event bindings and routing to handle the login and logout functionality.

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,17 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 import { RouterLink } from '@angular/router';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
     HlmButtonDirective,
     RouterLink,
-    RouterModule
+    RouterModule,
+    CommonModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit, OnDestroy {
+  userIsAuthenticated = false;
+  private authListenerSubs: Subscription | null = null;
+
+  constructor(private authService: AuthService) {
+    this.authListenerSubs = null;
+  }
+
+  ngOnInit() {
+    this.authListenerSubs = this.authService.getAuthStatusListener().subscribe(
+      isAuthenticated => {
+        this.userIsAuthenticated = isAuthenticated;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    if (this.authListenerSubs) {
+      this.authListenerSubs.unsubscribe();
+    }
+  }
+
+}
