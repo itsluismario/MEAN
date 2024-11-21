42d8a980ed113882507d556a2195edddff3ed473
itsluismario <itsluismario@gmail.com>
2024-11-20 18:45:17 -0600

    🐛 Correct Angular module imports
    - Remove non-module imports from AngularModule
    - Keep only proper Angular modules Technical changes:
    	- Remove FormControl, FormGroup, Validators from module imports
    	- Keep only NgModules (CommonModule, FormsModule, etc.)
    	- Fix Angular compiler errors

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,15 @@
+// src/app/shared/shared.module.ts
 import { NgModule } from '@angular/core';
+import { AngularModule } from './angular.module';
 import { SpartanModule } from './spartan.module';
 
 @NgModule({
   imports: [
+    AngularModule,
     SpartanModule
   ],
   exports: [
+    AngularModule,
     SpartanModule
   ]
 })
